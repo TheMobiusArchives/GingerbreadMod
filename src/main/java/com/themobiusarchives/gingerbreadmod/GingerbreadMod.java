bab0599c09c8f90b491796e69e184370a96bf4f1
TheMobiusArchives <TheMobiusArchives@gmail.com>
2014-12-04 20:33:48 -0800

    Update with version constant.

--- src/main/java/com/themobiusarchives/gingerbreadmod/GingerbreadMod.java
+++ src/main/java/com/themobiusarchives/gingerbreadmod/GingerbreadMod.java
@@ -7,7 +7,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version="1.7.10-1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class GingerbreadMod {
 	
 	@Mod.Instance("GingerbreadMod")
--- src/main/java/com/themobiusarchives/gingerbreadmod/reference/Reference.java
+++ src/main/java/com/themobiusarchives/gingerbreadmod/reference/Reference.java
@@ -3,4 +3,5 @@ package com.themobiusarchives.gingerbreadmod.reference;
 public class Reference {
 	public static final String MOD_ID = "GingerbreadMod";
 	public static final String MOD_NAME = "Gingerbread Mod";
+	public static final String VERSION = "1.7.10-1.0";
 }
