926e873fe452aaa0f08c826312d647a07e27cec6
TheMobiusArchives <TheMobiusArchives@gmail.com>
2014-12-04 20:19:23 -0800

    Basic mod class plus event stubs.

---
+++ src/main/java/com/themobiusarchives/gingerbreadmod/GingerbreadMod.java
@@ -0,0 +1,31 @@
+package com.themobiusarchives.gingerbreadmod;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="GingerbreadMod", name="Gingerbread Mod", version="1.7.10-1.0")
+public class GingerbreadMod {
+	
+	@Mod.Instance("GingerbreadMod")
+	public static GingerbreadMod instance;
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
+}
