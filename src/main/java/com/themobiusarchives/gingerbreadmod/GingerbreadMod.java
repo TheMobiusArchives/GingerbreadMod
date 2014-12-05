3b74516f522bfedbf9cd2795f5f252c280582fc8
TheMobiusArchives <TheMobiusArchives@gmail.com>
2014-12-04 20:31:42 -0800

    Adding new reference class.

--- src/main/java/com/themobiusarchives/gingerbreadmod/GingerbreadMod.java
+++ src/main/java/com/themobiusarchives/gingerbreadmod/GingerbreadMod.java
@@ -1,11 +1,13 @@
 package com.themobiusarchives.gingerbreadmod;
 
+import com.themobiusarchives.gingerbreadmod.reference.Reference;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid="GingerbreadMod", name="Gingerbread Mod", version="1.7.10-1.0")
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version="1.7.10-1.0")
 public class GingerbreadMod {
 	
 	@Mod.Instance("GingerbreadMod")
---
+++ src/main/java/com/themobiusarchives/gingerbreadmod/reference/Reference.java
@@ -0,0 +1,6 @@
+package com.themobiusarchives.gingerbreadmod.reference;
+
+public class Reference {
+	public static final String MOD_ID = "GingerbreadMod";
+	public static final String MOD_NAME = "Gingerbread Mod";
+}
